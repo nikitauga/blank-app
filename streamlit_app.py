2be07a8ea464c709a837d0b6c5d0e2a72ce538f3
nikitauga <60206546+nikitauga@users.noreply.github.com>
2024-07-17 12:33:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,45 @@
 import streamlit as st
+#Layout
+st.set_page_config(
+    page_title="SimiLo",
+    layout="wide",
+    initial_sidebar_state="expanded")
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+#Data Pull and Functions
+st.markdown("""
+<style>
+.big-font {
+    font-size:80px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+#Options Menu
+with st.sidebar:
+    selected = option_menu('SimiLo', ["Intro", 'Search','About'], 
+        icons=['play-btn','search','info-circle'],menu_icon='intersect', default_index=0)
+
+
+#Intro Page
+if selected=="Intro":
+    #Header
+    st.title('Welcome to SimiLo')
+    st.subheader('*A new tool to find similar locations across the United States.*')
+
+    st.divider()
+
+    #Use Cases
+    with st.container():
+        col1,col2=st.columns(2)
+        with col1:
+            st.header('Use Cases')
+            st.markdown(
+                """
+                - _Remote work got you thinking about relocation?_
+                - _Looking for a new vacation spot?_
+                - _Conducting market research for product expansion?_
+                - _Just here to play and learn?_
+                """
+                )
+
+
+    st.divider()
